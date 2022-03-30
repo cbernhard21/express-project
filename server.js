12166d39f2635fc8b240665f33241a4d9e53f6c5
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-03-30 20:57:17 +0000

    worked on middleware

--- server.js
+++ server.js
@@ -4,16 +4,48 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send({
+const friends = [
+    {
+        id: 0,
+        name: "Colin"
+    },
+    {
         id: 1,
-        name: 'Chris'
-    })
+        name: "William"
+    },
+    {
+        id: 2,
+        name: "Eli"
+    }
+];
+
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`method: ${req.method}, url: ${req.url} ${delta}ms`)
+});
+
+
+app.get('/friends', (req, res) => {
+    res.json(friends)
+});
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId]
+    if (friend) {
+        res.status(200).json(friend)
+    } else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
 })
 
 app.get('/messages', (req, res) => {
     res.send('<h1>hello messages</h1>')
-})
+});
 
 app.post('/messages', (req, res) => {
     console.log('updating messages....')
