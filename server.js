9fa7e6f04996efb84cbaeac71679c2d1d65ede0e
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-03-30 18:51:25 +0000

    first routes

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send({
+        id: 1,
+        name: 'Chris'
+    })
+})
+
+app.get('/messages', (req, res) => {
+    res.send('<h1>hello messages</h1>')
+})
+
+app.post('/messages', (req, res) => {
+    console.log('updating messages....')
+})
+
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
